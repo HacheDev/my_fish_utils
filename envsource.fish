18e87927dc34375ccc5b84f9be203e71e57e9682
HacheDev <javier.hernandez@with-madrid.com>
2023-04-21 17:17:54 +0200

    Now set options can be passed to envsource, if no options passed -Ux is used as the default

--- envsource.fish
+++ envsource.fish
@@ -5,12 +5,18 @@
 # Usage: envsource <path/to/env>
 
 function envsource
-  for line in (cat $argv | grep -v '^#' | grep -v '^\s*$' | grep -E '^\s*(.*)=.*$')
-    set item (string split -m 1 = $line)
-    set chars_to_remove "'\""
-    set first_char (echo $item[2] | grep -o "[$chars_to_remove]" | head -1)
-    set item[2] (echo $item[2] | string trim --chars=$first_char)
-    set -Ux $item[1] $item[2]
-    echo "Exported key $item[1] with value $item[2]"
-  end
+    set num_of_args (count $argv)
+    if test $num_of_args -gt 1 -a (string match -q -- -"$argv[1]")
+        set options $argv[1]
+    else
+        set options "-U"
+    end
+    for line in (cat $argv[2] | grep -v '^#' | grep -v '^\s*$' | grep -E '^\s*(.*)=.*$')
+        set item (string split -m 1 = $line)
+        set chars_to_remove "'\""
+        set first_char (echo $item[2] | grep -o "[$chars_to_remove]" | head -1)
+        set item[2] (echo $item[2] | string trim --chars=$first_char)
+        set $options $item[1] $item[2]
+        echo "Exported key $item[1] with value $item[2] and options $options"
+    end
 end
