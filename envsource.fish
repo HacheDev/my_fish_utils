5473c695c342959560b07cc5e0fae2448a54ae89
HacheDev <javier.hernandez@with-madrid.com>
2023-05-10 16:58:49 +0200

    If option passed is nota valid option, throws dummy error
    If option passed is not a valid option, throws dummy error
    
    If option passed is not a valid option, throws dummy error

--- envsource.fish
+++ envsource.fish
@@ -6,21 +6,21 @@
 
 function envsource
 
-    #First we save the number of arguments
+    #First we save the number of arguments and the valid options of the command
     set num_of_args (count $argv)
+    set valid_options b e g l p q t u U x
 
     # Check if number of args is 2
-    # if true, check if first argument starts with hyphen -
+    # if true, check if first argument starts with hyphen - and is a valid option of set command
     # if this is also true, check if the other arguments dont start with hyphen
-
-    if test $num_of_args -eq 2 -a (string match -q -- - $argv[1][1] -o $num_of_args -eq 1 -a ! string match -q -- - "$argv[2]")
+    if test $num_of_args -eq 2 && string match -rq -- "^-[$valid_options]" $argv[1] && ! string match -q -- "-*" $argv[2]
         # if every condition was true, set options to first argument and file to the second
         set options $argv[1]
         set file $argv[2]
 
     # Check that we are not passing only options without no file
     # so first argument can't start with a hyphen
-    else if test $num_of_args -eq 1 -a (! string match -q -- - $argv[1])
+    else if test $num_of_args -eq 1 && ! string match -q -- "-*" $argv[1]
         set options -gx
         set file $argv[1]
 
