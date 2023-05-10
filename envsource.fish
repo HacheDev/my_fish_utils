7222d2f67e30c43605f916e40cb7c1d8dd344c74
HacheDev <javier.hernandez@with-madrid.com>
2023-05-10 12:32:17 +0200

    Now envsource supports every option that is included in set command

--- envsource.fish
+++ envsource.fish
@@ -5,31 +5,26 @@
 # Usage: envsource <path/to/env>
 
 function envsource
-  # Check if number of args is greater than 1
+
+    #First we save the number of arguments
+    set num_of_args (count $argv)
+
+    # Check if number of args is 2
     # if true, check if first argument starts with hyphen -
-    # if this is also true, check if the other arguments dont start with arguments
-    if test $num_of_args -ge 2 && string match -q -- - $argv[1][1] && \
-        begin
-        test $num_of_args -eq 2 || not string match -q -- - "$argv[2]"
-        end && \
-        begin
-        test $num_of_args -le 2 || not string match -q -- - "$argv[2..-1]"
-        end
+    # if this is also true, check if the other arguments dont start with hyphen
+
+    if test $num_of_args -eq 2 -a (string match -q -- - $argv[1][1] -o $num_of_args -eq 1 -a ! string match -q -- - "$argv[2]")
         # if every condition was true, set options to first argument and file to the second
         set options $argv[1]
         set file $argv[2]
-        echo "Im here"
-        
+
     # Check that we are not passing only options without no file
     # so first argument can't start with a hyphen
-    else if not string match -q -- - $argv[1]
+    else if test $num_of_args -eq 1 -a (! string match -q -- - $argv[1])
         set options -gx
-        echo "im here"
         set file $argv[1]
-        echo $options
-        echo $file
 
-        # If we didnt pass the checks, return error
+    # If we didnt pass the checks, return error
     else
         echo "Something went wrong"
         return 1
@@ -45,15 +40,24 @@ function envsource
         # Split line between variable and value
         set item (string split -m 1 = $line)
 
-        # Remove quotes or double quotes from values when they surround it
-        # (since fish does not treat them as special chars when using set command)
-        # It only removes the first of them that is found on the value
-        set chars_to_remove "'\""
-        set first_char (echo $item[2] | grep -o "[$chars_to_remove]" | head -1)
-        set item[2] (echo $item[2] | string trim --chars=$first_char)
+        # If option -e was passed, we notify we are erasing variables
+        # We do this check before to avoid performing useless operations
+        # the values of the env files since they wont be used
+        if test "$options" = "-e"
+            set $options $item[1]
+            echo "Deleted variable $item[1]"
 
-        # Set the shell variable with the options passed (or the default -gx)
-        set $options $item[1] $item[2]
-        echo "Exported key $item[1] with value $item[2] and options $options"
+        else
+            # Remove quotes or double quotes from values when they surround it
+            # (since fish does not treat them as special chars when using set command)
+            # It only removes the first of them that is found on the value
+            set chars_to_remove "'\""
+            set first_char (echo $item[2] | grep -o "[$chars_to_remove]" | head -1)
+            set item[2] (echo $item[2] | string trim --chars=$first_char)
+
+            # Set the shell variable with the options passed (or the default -gx)
+            set $options $item[1] $item[2]
+            echo "Exported variable $item[1] with value $item[2] and options $options"
+        end
     end
 end
