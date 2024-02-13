2f7644357a4b52327fd91c1f09c0e94cc7afd357
HacheDev <javier.hernandez@with-madrid.com>
2024-02-13 12:52:54 +0100

    Chore: fix bugs. Todo: check why fish script always gives more cpu usage than dash and bash scripts

--- auto_killer/auto_killer.bash
+++ auto_killer/auto_killer.bash
@@ -3,7 +3,7 @@
 # Default values
 num_cores=12
 ram_threshold=90  # This will be the total RAM usage threshold
-cpu_per_core_threshold=90 # This will be the CPU usage threshold per core
+cpu_per_core_threshold=90 # This will be the total CPU usage threshold per core
 
 # Parse arguments
 for arg in "$@"
@@ -25,9 +25,6 @@ do
     esac
 done
 
-# Calculate CPU Usage Threshold
-cpu_threshold=$((num_cores * cpu_per_core_threshold ))
-
 # Log path
 log_path='/var/log/auto_killer_log'
 
@@ -36,39 +33,50 @@ check_repeating_time=3
 
 while true
 do
+    # Calculate total RAM usage using free command
+    read total_ram available_ram <<< $(LC_ALL=C free | awk '/Mem:/{print $2, $7}')
+    echo "Total RAM: $total_ram, Available RAM: $available_ram"
+
     # Calculate total RAM usage
-    total_ram_usage=$(ps -aux | awk '{sum += $4} END {print sum}')
+    total_ram_usage=$((total_ram - available_ram))
+    total_ram_usage_percentage=$(LC_ALL=C awk "BEGIN {printf \"%.2f\", $total_ram_usage * 100 / $total_ram}")
+
+    echo "Total RAM usage: $total_ram_usage, Total RAM usage percentage: $total_ram_usage_percentage%"
+
+    # Check if total RAM usage is greater than the threshold
+    if (( $(echo "$total_ram_usage_percentage > $ram_threshold" | bc -l) )); then
+        echo "Total RAM usage ($total_ram_usage_percentage%) exceeds threshold ($ram_threshold%). Proceeding to kill processes with highest RAM usage."
+
+        # Find the process with the highest RAM usage
+        read highest_ram_pid highest_ram_usage <<< $(ps --sort=-%mem -eo pid,%mem | awk 'NR==2 {print $1, $2}')
+
+        # Kill the process and subprocesses
+        echo "Killing process with PID $highest_ram_pid and RAM usage $highest_ram_usage and its subprocesses"
+        pkill -TERM -P $highest_ram_pid
+        kill -TERM $highest_ram_pid
+        echo "Process $highest_ram_pid killed."
+    fi
 
     # Calculate total CPU usage
     total_cpu_usage=$(ps -aux | awk '{sum += $3} END {print sum}')
+#    total_cpu_usage_percentage_adjusted=$(LC_ALL=C awk "BEGIN {printf \"%.2f\", $total_cpu_usage / $num_cores}")
+    total_cpu_usage_percentage_adjusted=$(LC_ALL=C awk -v total="$total_cpu_usage" -v cores="$num_cores" 'BEGIN {printf "%.2f", total/cores}')
 
-    # Check if total RAM usage is greater than the threshold
-    if (( $(echo "$total_ram_usage > $ram_threshold" | bc -l) ))
-    then
-        echo "Total RAM usage ($total_ram_usage%) exceeds threshold ($ram_threshold%). Consider taking action."
-
-        # Find the process with the highest RAM usage
-        read highest_ram_pid highest_ram_usage <<< $(ps -aux --sort=-%mem | awk 'NR==2{print $2, $4}')
-
-        # Kill the process
-        echo "Killing process with PID $highest_ram_pid and RAM usage $highest_ram_usage%"
-        kill -9 $highest_ram_pid
-        echo "Process ($highest_ram_pid) killed."
-    fi
+    echo "Total CPU usage percentage: $total_cpu_usage%"
+    echo "Total CPU usage per core: $total_cpu_usage_percentage_adjusted%"
 
     # Check if total CPU usage is greater than the threshold
-    total_cpu_usage_adjusted=$(echo "$total_cpu_usage / $num_cores" | bc -l)
-    if (( $(echo "$total_cpu_usage_adjusted > $cpu_per_core_threshold" | bc -l) ))
-    then
-        echo "Total CPU usage per core ($total_cpu_usage_adjusted%) exceeds threshold ($cpu_per_core_threshold%). Consider taking action."
+    if (( $(echo "$total_cpu_usage_percentage_adjusted > $cpu_per_core_threshold" | bc -l) )); then
+        echo "Total CPU usage per core ($total_cpu_usage_percentage_adjusted%) exceeds threshold ($cpu_per_core_threshold%). Proceeding to kill processes with highest CPU usage."
 
         # Find the process with the highest CPU usage
-        read highest_cpu_pid highest_cpu_usage <<< $(ps -aux --sort=-%cpu | awk 'NR==2{print $2, $3}')
+        read highest_cpu_pid highest_cpu_usage <<< $(ps --sort=-%cpu -eo pid,%cpu | awk 'NR==2 {print $1, $2}')
 
-        # Kill the process
-        echo "Killing process with PID $highest_cpu_pid and CPU usage $highest_cpu_usage%"
-        kill -9 $highest_cpu_pid
-        echo "Process ($highest_cpu_pid) killed."
+        # Kill the process and subprocesses
+        echo "Killing process with PID $highest_cpu_pid and CPU usage $highest_cpu_usage and its subprocesses"
+        pkill -TERM -P $highest_cpu_pid
+        kill -TERM $highest_cpu_pid
+        echo "Process $highest_cpu_pid killed."
     fi
 
     sleep $check_repeating_time
--- auto_killer/auto_killer.fish
+++ auto_killer/auto_killer.fish
@@ -4,6 +4,8 @@
 set num_cores 12
 set ram_threshold 90  # This will be the total RAM usage threshold
 set cpu_per_core_threshold 90 # This will be the total CPU usage threshold per core
+set -gx LC_NUMERIC "C"
+
 
 # Parse arguments
 for arg in $argv
@@ -30,32 +32,42 @@ set log_path '/var/log/auto_killer_log'
 set check_repeating_time 3
 
 while true
-    # Calculate total RAM usage
-    set total_ram_usage (ps -aux | awk '{sum += $4} END {print sum}')
+    # Calculate total RAM usage using free command
+    set mem_info (free | awk '/Mem:/{print $2, $7}')
+    set total_ram (echo $mem_info | awk '{print $1}')
+    set available_ram (echo $mem_info | awk '{print $2}')
+    echo "Total RAM: $total_ram, Available RAM: $available_ram"
 
-    # Calculate total CPU usage
-    set total_cpu_usage (ps -aux | awk '{sum += $3} END {print sum}')
+    set total_ram_usage (math "$total_ram - $available_ram")
+    set total_ram_usage_percentage (math "($total_ram_usage / $total_ram) * 100")
+    echo "Total RAM usage: $total_ram_usage%, Total RAM usage percentage: $total_ram_usage_percentage%"
 
     # Check if total RAM usage is greater than the threshold
-    if test $total_ram_usage -gt $ram_threshold
-        echo "Total RAM usage ($total_ram_usage%) exceeds threshold ($ram_threshold%). Consider taking action.
-        Proceeding to kill processes..."
+    if test $total_ram_usage_percentage -gt $ram_threshold
+        echo "Total RAM usage ($total_ram_usage_percentage%) exceeds threshold ($ram_threshold%). Proceeding to kill processes with highest RAM usage."
 
         # Find the process with the highest RAM usage
         set highest_ram_process (ps -aux --sort=-%mem | awk 'NR==2{print $2, $4}')
         set highest_ram_pid (echo $highest_ram_process | awk '{print $1}')
         set highest_ram_usage (echo $highest_ram_process | awk '{print $2}')
 
-        # Kill the process
-        echo "Killing process with PID $highest_ram_pid and RAM usage $highest_ram_usage%"
+        # Kill the process and subproceses
+        echo "Killing process with PID $highest_ram_pid and RAM usage $highest_ram_usage and its subprocesses"
+        pkill -P -9 $highest_ram_pid
         kill -9 $highest_ram_pid
-        echo "Process ($highest_ram_pid%) killed."
+        echo "Process $highest_ram_pid killed."
     end
 
-    # Check if total CPU usage is greater than the threshold
-    set total_cpu_usage_adjusted (math "$total_cpu_usage / $num_cores")
-    if test $total_cpu_usage_adjusted -gt $cpu_threshold
-        echo "Total CPU usage per core ($total_cpu_usage_adjusted%) exceeds threshold ($cpu_per_core_threshold%). Consider taking action."
+    # Calculate total CPU usage
+    set total_cpu_usage (ps -aux | awk '{sum += $3} END {print sum}')
+    set total_cpu_usage_percentage_adjusted (math "$total_cpu_usage / $num_cores")
+    set total_cpu_usage_percentage_adjusted (printf "%.2f" $total_cpu_usage_percentage_adjusted)
+
+    echo "Total CPU usage percentage: $total_cpu_usage%"
+    echo "Total CPU usage per core: $total_cpu_usage_percentage_adjusted%"
+
+    if test $total_cpu_usage_percentage_adjusted -gt $cpu_threshold
+        echo "Total CPU usage per core ($total_cpu_usage_percentage_adjusted%) exceeds threshold ($cpu_per_core_threshold%). Proceeding to kill processes with highest CPU usage."
 
         # Find the process with the highest CPU usage
         set highest_cpu_process (ps -aux --sort=-%cpu | awk 'NR==2{print $2, $3}')
@@ -63,9 +75,10 @@ while true
         set highest_cpu_usage (echo $highest_cpu_process | awk '{print $2}')
 
         # Kill the process
-        echo "Killing process with PID $highest_cpu_pid and CPU usage $highest_cpu_usage%"
+        echo "Killing process with PID $highest_cpu_pid and CPU usage $highest_cpu_usage and its subprocesses"
+        pkill -P -9 $highest_cpu_pid
         kill -9 $highest_cpu_pid
-        echo "Process ($highest_cpu_pid%) killed."
+        echo "Process $highest_cpu_pid killed."
     end
 
     sleep $check_repeating_time
